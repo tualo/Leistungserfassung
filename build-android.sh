1fcd3677e3f7dfadc21cea150cc6e831debbba2a
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2014-04-09 08:18:21 +0200

    clean up code base

--- build-android.sh
+++
@@ -1,7 +0,0 @@
-export ANDROID_NDK_ROOT=/Users/thomashoffmann/AndroidDEV/ndk
-export ANDROID_SDK_ROOT=/Users/thomashoffmann/AndroidDEV/sdk
-export PATH=$PATH:/Users/thomashoffmann/AndroidDEV/sdk/tools/:/Users/thomashoffmann/AndroidDEV/sdk/platform-tools/:/Users/thomashoffmann/Qt5.2.1/5.2.1/android_armv7/bin
-qmake
-make install INSTALL_ROOT=android-build/
-androiddeployqt --output android-build/ --release --sign /Users/thomashoffmann/androidworkspace/tualo-keystore2 tualo
-make clean
